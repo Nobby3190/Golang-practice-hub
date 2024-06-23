686ca98d9492485546bf55ffc2acc92b4e3c9f03
nobby <zuzu60410@gmail.com>
2024-06-23 03:15:19 +0000

    First commit

---
+++ src/gin-gonic/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// type Data struct {
+// 	Ping  string `json:"ping"`
+// 	Hello string `json:"Hello"`
+// }
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	server.GET("/add", func(ctx *gin.Context) {
+		aStr := ctx.Query("a")
+		bStr := ctx.Query("b")
+
+		a, errA := strconv.Atoi(aStr)
+		b, errB := strconv.Atoi(bStr)
+
+		if errA != nil || errB != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid parameters",
+			})
+			return
+		}
+
+		sum := a + b
+		ctx.JSON(http.StatusOK, gin.H{
+			"Sum": sum,
+		})
+	})
+
+	server.Run()
+}
